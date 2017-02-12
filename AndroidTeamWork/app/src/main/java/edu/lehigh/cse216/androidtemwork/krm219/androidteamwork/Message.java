2ddb8f1ce57a68b12e21eaef4ebcac393740b5d0
Krishan300 <krishanmadan97@gmail.com>
2017-02-11 19:35:04 -0500

    firstcommit

---
+++ AndroidTeamWork/app/src/main/java/edu/lehigh/cse216/androidtemwork/krm219/androidteamwork/Message.java
@@ -0,0 +1,12 @@
+package edu.lehigh.cse216.androidtemwork.krm219.androidteamwork;
+
+/**
+ * Created by krishanmadan on 2/11/17.
+ */
+
+public class Message {
+    private int likes;
+    private int datecreated;
+    private String title;
+    private String content;
+}
