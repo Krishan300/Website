7fb5bd00b4a349f0b7caafa9689730220eb4fb87
Krishan300 <krishanmadan97@gmail.com>
2017-02-16 10:50:10 -0500

    initial commit

--- AndroidTeamWork/app/src/main/java/edu/lehigh/cse216/androidtemwork/krm219/androidteamwork/MainActivity.java
+++ AndroidTeamWork/app/src/main/java/edu/lehigh/cse216/androidtemwork/krm219/androidteamwork/MainActivity.java
@@ -1,30 +1,54 @@
 package edu.lehigh.cse216.androidtemwork.krm219.androidteamwork;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     //declare variables
     //this is a  test comment
-    private TextView mfactTextView;
-    private Button mLikeButton;
-    ArrayList<Message> listofmessages=new ArrayList<Message>();
+   // private TextView mfactTextView;
+    public Button mLaunchMessage;
+
+    private EditText GetMessage;
+
+    //private EditText GetName;
+    //private EditText GetDate;
+
+    //ArrayList<Message> listofmessages=new ArrayList<Message>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        mLaunchMessage=(Button) findViewById(R.id.button3);
+        GetMessage=(EditText) findViewById(R.id.editText);
+       // GetName=(EditText) findViewById(R.id.NameEditText);
+        //GetDate=(EditText) findViewById(R.id.DateEditText);
+
+        mLaunchMessage.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View w)
+            {
+                String MessageText=GetMessage.getText().toString();
+                //Toast.makeText(MainActivity.this, MessageText, Toast.LENGTH_LONG).show();
+                Intent launchMessage=new Intent(MainActivity.this, ActivityMessage.class);
+                launchMessage.putExtra(getString(R.string.messagetext), MessageText);
+                startActivity(launchMessage);
+               /* String Name=GetName.getText().toString();
+                launchMessage.putExtra("Name", Name);
+                String Date=GetDate.getText().toString();
+                launchMessage.putExtra("Date", Date);*/
+            }
+        });
+        //Toolbar toolbar = (Toolbar) findViewById(toolbar);
+        //setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -34,8 +58,11 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
              //assign views from layout file to corresponding file
                // mfactTextView = findViewById(int id);
+
             }
         });
+
+
     }
 
     @Override
