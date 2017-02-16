c21a9947de276d737b1d04520db3b85ea8d5d61c
cam418 <cam418@lehigh.edu>
2017-02-15 21:54:00 -0500

    inital commit test

---
+++ phase1-backend/src/main/java/backend/luna/lehigh/edu/App.java
@@ -0,0 +1,13 @@
+package backend.luna.lehigh.edu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
