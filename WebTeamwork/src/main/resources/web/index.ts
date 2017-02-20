c83ee1acf76ea6ce5c1f9a57521399335f109696
Robert Salay <Robert Salay>
2017-02-20 18:50:25 -0500

    final commit

--- WebTeamwork/src/main/resources/web/index.ts
+++ WebTeamwork/src/main/resources/web/index.ts
@@ -1,22 +1,28 @@
 /**
  * Created by Robert Salay on 2/12/2017.
  */
+/** Creates a comment input box, allows for title & comment
+ *boxDisplay returns html code for displaying class on a web page
+ *sendComment sends a post route to the database
+ */
     class CommentBox{
-        constructor(){
-
-        }
-
-        //display comment creation box
+        /**
+         *
+         * @returns {html to display object on web page}
+         */
         boxDisplay(){
             return "<input id='newTitle' size='50' maxlength='50' type='text' value='A title for your post! 50 characters max.'>"
             + " </input>"
             + "<textarea id='newComment' rows='4' cols='50' maxlength='144'>"
-            + "Share your thoughts with the world! 144 characters max, please. </textarea>" +
-            "<button id='sendComment' onclick='sendComment()'>Send Comment</button>"
+            + "Share your thoughts with the world! 144 characters max, please. </textarea>"
+                + "<button id='sendComment' onclick='sendComment()'>Send Comment</button>"
         }
 
-        //sends custom comment and title to db, checks for null
-        sendComment(){
+    /**
+     * takes data from textboxes and converts it into a full json object, sent to the db via a POST route
+     * shows an error if it can't send the data, otherwise reloads the page via getDataFromServer
+     */
+    sendComment(){
             var title = document.getElementById("newTitle").textContent;
             var comment = document.getElementById("newComment").textContent;
             title = (title === "") ? null : title;
@@ -40,7 +46,21 @@
         }
 
     }
+    /** Creates an individual comment in a comment list
+    *@param index number describing comment
+    *@param title title of comment
+    *@param comment text of comment
+    *@param likes number of likes/dislikes on comments
+    *@param uploadDate Date object indicating time uploaded
+    *@param likeDate Date object indicating last time voted upon
+    *
+    *commentDisplay returns html code in a string detailing how to display the object on a web page
+    *likeClick and dislikeClick both send a put route that calls for their comment's like and likeDate parameters to be altered
+     */
     class CommentList{
+        /**
+         *initializes the parameters for a CommentList object
+         */
         constructor(public index: number,
                     public title: string,
                     public comment: string,
@@ -49,7 +69,10 @@
                     public likeDate: Date) {
         }
 
-        //displays comment
+        /**
+         *
+         * @returns {html code to display CommentList on a web page}
+         */
         commentDisplay() {
             return "<hr> <h1>" + this.title + "</h1>"
                 + "<p>" + this.comment + "</p><br>"
@@ -59,11 +82,14 @@
                 + "<p> and was last liked on </p>" +  this.likeDate + "<br>";
         }
 
-        //sends message to db about specific comment being liked
+        /**
+         * updates a comment's like # and like date via a PUT route.
+         * shows an error if it cannot do so, otherwise reloads the page via getDataFromServer
+         */
         likeClick() {
             $.ajax({
                 type: "PUT",
-                url: "data/like/" + this.index + "/upvote",
+                url: "data/like/up/" + this.index.toString(),
                 data: JSON.stringify({"index":this.index, "title":this.title, "comment":this.comment, "uploadDate":this.uploadDate, "likeDate":Date.now()}),
                 dataType: "json",
                 success: function (data) {
@@ -76,11 +102,15 @@
                 }
             });
         }
-        //sends message to db about specific comment being disliked
+
+        /**
+         * updates a comment's like # and like date via a PUT route.
+         * shows an error if it cannot do so, otherwise reloads the page via getDataFromServer
+         */
         dislikeClick() {
             $.ajax({
                 type: "PUT",
-                url: "data/like/" +this.index +"/downvote",
+                url: "data/like/down/" + this.index.toString(),
                 data: JSON.stringify({"index":this.index, "title":this.title, "comment":this.comment, "uploadDate":this.uploadDate, "likeDate":Date.now()}),
                 dataType: "json",
                 success: function (data) {
@@ -94,34 +124,45 @@
             });
         }
     }
+
+    /**
+    *requests data from db as a json object, resets the html, and turns json data into an array of commentList objects and displays them
+     */
     function getDataFromServer() {
         //clear html
-        document.getElementById("header").innerHTML = "";
         document.getElementById("commentBox").innerHTML = "";
         document.getElementById("listing").innerHTML = "";
 
-        var a = new CommentBox();
-        document.getElementById("commentBox").innerHTML = a.boxDisplay(); //display comment box
+        var cBox = new CommentBox();
+        document.getElementById("commentBox").innerHTML = cBox.boxDisplay(); //display comment box
+        /**
+         * uses GET route to obtain data from db as JSON.
+         * if possible, turns data into an array of commentList objects and displays them in reverse chronological order
+         * if not, sends an error
+         */
         $.ajax({
             type: "GET",
             url: "/data",
             dataType: "json",
             success: function (data) {
-                var b = new CommentList[data.length];
-                //turn db data into comments stored in reverse chronological order
-                for (var i in data) {
+                if (data.res === "okay") {
+                    var cList = new CommentList[data.length];
+                    //turn db data into comments stored in reverse chronological order
                     var counter = 1;
-                    b[data.length - counter] = new CommentList(data[i].index, data[i].title, data[i].comment, data[i].likes, data[i].uploadDate, data[i].likeDate);
-                    counter++;
-                }
-                //display all comments
-                for (var _i = 0, b_1 = b; _i < b_1.length; _i++) {
-                    var j = b_1[_i];
-                    document.getElementById("listing").innerHTML += j.commentDisplay();
+                    for (let i in data) {
+                        cList[data.length - counter] = new CommentList(data[i].index, data[i].title, data[i].comment, data[i].likes, data[i].uploadDate, data[i].likeDate);
+                        counter++;
+                    }
+                    //display all comments
+                    for (var k = 0; k < cList.length; k++) {
+                        document.getElementById("listing").innerHTML += cList[k].commentDisplay();
+                    }
+                } else {
+                    window.alert("Cannot get web data at this time. Try again later.")
                 }
             }
         });
 
     }
-    document.getElementById("header").innerHTML = "<h1 align='center'>The Buzz:</h1>  <h2 align='center'>The newest social media out there!</h2> <hr>";
-    getDataFromServer();
+//calls getDataFromServer to initialize page
+getDataFromServer();
